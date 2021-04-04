e26178f25c82c9be63070743961bc9292b402404
alessondejesus <asn9006@hotmail.com>
2021-04-03 22:11:59 -0500

    feat: setup projetos

---
+++ lib/src/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const basePrimaryColor = Color(0xFFFF7643);
+const basePrimaryLightColor = Color(0xFFFFECDF);
+const basePrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
+);
+const baseSecondaryColor = Color(0xFF979797);
+const baseTextColor = Color(0xFF757575);
+const baseAnimationDuration = Duration(milliseconds: 200);
